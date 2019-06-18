9d3c8b19f174809c82bc2024bf3f5e2623a67514
jdavidg1 <jdavidg_1@hotmail.com>
2019-06-18 16:51:01 -0500

    Programa final

--- Dividendo/Program.cs
+++ Dividendo/Program.cs
@@ -10,9 +10,29 @@ namespace Dividendo
     {
         static void Main(string[] args)
         {
+            //Declaración de variables
 
+            float dividendo = 0.0f;
+            float divisor = 0.0f;
+            float resultado = 0.0f;
 
+            Console.WriteLine("Cual es el dividendo ");
+            dividendo = Convert.ToSingle(Console.ReadLine());
 
+            Console.WriteLine("cual es el divisor ");
+            divisor = Convert.ToSingle(Console.ReadLine());
+
+            if(divisor!=0)
+            {
+
+                resultado = dividendo / divisor;
+
+                Console.WriteLine("El resultado de dividir {0} entre {1} es {2}", dividendo, divisor, resultado);
+
+            }
+            
+            Console.ReadKey();
+            
         }
     }
 }
